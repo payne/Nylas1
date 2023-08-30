3a77e716642e28f5e0a846db27d381a1ccf798b6
Matt Payne <Payne@MattPayne.org>
2023-08-29 19:17:00 -0500

    Sending hyperlink works ok, so I think we can build a simple RSVP with this.

---
+++ 2_try_hyperlink.sh
@@ -0,0 +1,21 @@
+curl -X POST \
+'https://api.nylas.com/send' \
+-H "Authorization: Bearer ${AT}" \
+-H 'Content-Type: application/json' \
+-H 'cache-control: no-cache' \
+--data-raw '{
+  "subject": "From Nylas",
+  "to": [
+    {
+      "email": "matt.g.payne@gmail.com",
+      "name": "Matt Payne"
+    }
+  ],
+  "from": [
+    {
+      "email": "matt.busy.payne@gmail.com",
+      "name": "Matt Busy Payne"
+    }
+  ],
+  "body": "<a href=https://MattPayne.org>This email was sent using the Nylas</a> email API."
+}'
